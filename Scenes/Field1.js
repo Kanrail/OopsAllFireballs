90232dfc9a5cc0eddbc74cb656a9a6d7d69b5099
Daniel Bowen <generalkan@hotmail.com>
2022-03-26 16:21:57 -0500

    Movement and fireball firing complete, still need to add emitter for fireball explosion and physics check for object collection with enemies.

--- Scenes/Field1.js
+++ Scenes/Field1.js
@@ -4,10 +4,17 @@ class Field1 extends Phaser.Scene{
         super("Field1");
         this.player = null;
         this.playfield = null;
+
+        this.fireballCooldown = 500;
+        this.lastFireball = Date.now();
     }
     preload(){
         this.load.image('playfield','Assets/Environment/PlayField.png');
-        this.load.image('player','Assets/Player/Player_Idle_1.png')
+        this.load.image('player','Assets/Player/Player_Idle_1.png');
+        this.load.spritesheet('fireball','Assets/Player/FireBall_Attack.png',{
+            frameWidth:100,
+            frameHeight:100
+        })
     }
 
     create(){
@@ -43,14 +50,56 @@ class Field1 extends Phaser.Scene{
         })
 
         //Player Attack
+        this.anims.create({
+            key:"fireball",
+            frameRate: 10,
+            frames:this.anims.generateFrameNumbers("fireball",{start:0,end:3}),
+            repeat:-1
+        });
+
         this.input.on('pointerdown', (pointer)=>{
             var touchX = pointer.x;
             var touchY = pointer.y;
-            var explosion = this.add.circle(touchX,touchY,80,0xFF0000);
+            var angleFromPlayer = Phaser.Math.Angle.Between(touchX,touchY,this.player.x, this.player.y);
+
+            console.log(angleFromPlayer);
+            if(this.lastFireball + this.fireballCooldown < Date.now()){
+                this.lastFireball = Date.now();
+                var fireball = this.physics.add.sprite(this.player.x,this.player.y,'fireballSet')
+                fireball.setScale(.5);
+                fireball.setAngle(Phaser.Math.RadToDeg(angleFromPlayer));
+                this.physics.moveTo(fireball,touchX,touchY,100);
+                fireball.anims.play('fireball');
+
+                let fInterval = setInterval(()=>{
+                    if(Math.abs(fireball.x - touchX) <= 20 && Math.abs(fireball.y - touchY) <= 200){
+                        var explosion = this.add.circle(fireball.x,fireball.y,80,0xFF0000);
+                        this.tweens.add({
+                            targets:explosion,
+                            duration: 1000,
+                            delay:0,
+                            alpha:0,
+                            repeat:0,
+                            yoyo:false,
+                            onComplete: ()=>{
+                                explosion.destroy();
+                            }
+                        })
+                        fireball.destroy();
+                        fInterval = clearInterval(fInterval);
+                    }
+                    console.log('Still going');
+                },50)
+
+
+            }
+            else{
+                console.log('Not Firing')
+            }
         })
     }
 
     update(){
-
+        
     }
 }
