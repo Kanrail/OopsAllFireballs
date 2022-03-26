9aed3d32e40f5e1c3fcdd84aa75cf5e351b2ae11
Daniel Bowen <generalkan@hotmail.com>
2022-03-25 20:02:42 -0500

    Adding placeholder start screen assets

--- Scenes/TitleScene.js
+++ Scenes/TitleScene.js
@@ -1,10 +1,12 @@
 class TitleScene extends Phaser.Scene{
   constructor(){
     super("TitleScene");
-    
+    this.startbutton = null;
+    this.background = null;
 }
   preload(){
-
+    this.load.image('background','Assets/StartScreen.png');
+    this.load.image('startButton','Assets/StartButton.png');
   }
   create(){
 
